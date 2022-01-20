1f98075b2a7668c64c60aaaea1d3d019da1b310c
JoJosmin <ead3070@naver.com>
2022-01-20 19:36:23 +0900

    프로필 페이지

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,7 @@
-const Home = () => {
-    return (
-        <div>
-            <h2>HOME</h2>
-        </div>
-    );
-};
+import React from "react";
+const Home = () => <div>
+    <form>
+        <imput type="text" placeholder="Write your bucketlist" maxLength={120} />
+    </form>
+</div>
 export default Home;
