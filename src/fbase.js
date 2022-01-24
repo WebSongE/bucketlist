29f946deaa75c468f3bd8eb2a3d28c02f02179bb
Monu <kdie44@gmail.com>
2022-01-24 19:30:54 +0900

    firebase 설정키 보안

--- src/fbase.js
+++ src/fbase.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC4LoKyuGSomTvuRA7uT5aXOrh4WQbic2w",
-    authDomain: "bucketlist-ef183.firebaseapp.com",
-    projectId: "bucketlist-ef183",
-    storageBucket: "bucketlist-ef183.appspot.com",
-    messagingSenderId: "60967211945",
-    appId: "1:60967211945:web:7499f6c64ed47f1c07eab8"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
 
 const app=initializeApp(firebaseConfig);
