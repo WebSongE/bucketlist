7a11158ea607ca79ef941171c02e917aa46ce1dd
JoJosmin <ead3070@naver.com>
2022-04-05 14:41:57 +0900

    _delegate 에러

--- src/components/AddBucket.js
+++ src/components/AddBucket.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import {collection,doc,updateDoc,getFirestore,getDoc, setDoc, addDoc} from "firebase/firestore";
-//import { dbService } from "fbase";
+import { dbService } from "fbase";
 
 const AddBucket = ({userObj}) => {
     const [newBucket, setNewBucket] = useState("");
@@ -14,8 +14,8 @@ const AddBucket = ({userObj}) => {
 
     useEffect(()=>{
         const getTags=async()=>{
-            const tagRef=addDoc(db,"userAllTags",userObj.id);
-            const data=await getDoc(tagRef);
+            const tagRef= addDoc(db,"userAllTags",userObj.id)||'-';
+            const data=await getDoc(tagRef)||'-';
             if(data.exists()){
                 setUserTags(data);
             }
@@ -69,7 +69,7 @@ const AddBucket = ({userObj}) => {
         tagArray.forEach((item)=>{
             if(userTags.has(item)===false) userTags.set(item,true);
         });
-        updateDoc(doc(getFirestore(),"userContents",userObj.id),{userAllTags:userTags});
+        updateDoc(addDoc(dbService,"userContents",userObj.id),{userAllTags:userTags});
         if(collection("userContents"))
         allInit();
     };
