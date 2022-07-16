9c6dbec97daefc970692ad8e504b1911f1498c9b
KimDahui42 <kdie44@gmail.com>
2022-07-16 23:03:08 +0900

    open user

--- src/components/AddBucket.js
+++ src/components/AddBucket.js
@@ -1,92 +1,108 @@
 import { useEffect, useState } from "react";
-import {collection,updateDoc,getFirestore,getDoc, setDoc, addDoc,doc} from "firebase/firestore";
-import { dbService } from "fbase";
+import {
+  collection,
+  getFirestore,
+  getDoc,
+  addDoc,
+  doc,
+} from "firebase/firestore";
 
-const AddBucket = ({userObj}) => {
-    const [newBucket, setNewBucket] = useState("");
-    const [tags, setNewTags] = useState("");
-    const [tagArray,setTagArray]=useState([]);
-    const [userTags,setUserTags]=useState(new Map());
-    const [expiredDate,setNewExpiredDate]=useState(new Date());
+const AddBucket = ({ userObj }) => {
+  const [newBucket, setNewBucket] = useState("");
+  const [tags, setNewTags] = useState("");
+  const [tagArray, setTagArray] = useState([]);
+  const [userTags, setUserTags] = useState(new Map());
+  const [expiredDate, setNewExpiredDate] = useState(new Date());
 
-    const db=getFirestore();
-    const bucketRef=collection(db,"users/"+userObj.uid+"/buckets");
-    
-    useEffect(()=>{
-        const getTags=async()=>{
-            const tagRef=doc(getFirestore(),"users/"+userObj.uid);
-            const temp=await getDoc(tagRef);
-            setTagArray(temp.data().userAllTags);
-        }
-        getTags();
-    },[]);
-    const getTags=async()=>{
-        const tagRef=doc(getFirestore(),"users/"+userObj.uid);
-        const temp=await getDoc(tagRef);
-        console.log("getTags");
-        setTagArray(temp.data().userAllTags);
-    }
+  const db = getFirestore();
+  const bucketRef = collection(db, "users/" + userObj.uid + "/buckets");
 
-    const onChange = (event) => {
-        event.preventDefault();
-        const {
-            target: { value },
-        } = event;
-        setNewBucket(value);
-    }
-    const onChangeDate = (event) => {
-        event.preventDefault();
-        const {
-            target: { value },
-        } = event;
-        setNewExpiredDate(value);
-    }
-    const onChangeTags = (event) => {
-        event.preventDefault();
-        const {
-            target: { value },
-        } = event;
-        setNewTags(value);
-    }
-    const allInit=()=>{
-        console.log("all init");
-        setNewBucket("");
-        setNewTags("");
-        getTags();
-        setUserTags(new Map());
-    }
-    const onSubmit = async (event) => {
-        event.preventDefault();
-        console.log("onSubmit");
-        if (newBucket === "") return;
-        setTagArray(tags.split('#').join(',').split(' ').join(',').split(','));
-        console.log(tagArray);
-        await addDoc(bucketRef,{
-            text: newBucket,
-            dateAt: Date.now(),
-            expiredAt: expiredDate,
-            userId: userObj.uid,
-            tags: tagArray
-        });
-        tagArray.forEach((item)=>{
-            if(userTags.has(item)===false) userTags.set(item,true);
-        });
-        console.log("successed");
-        allInit();
+  useEffect(() => {
+    const getTags = async () => {
+      const tagRef = doc(getFirestore(), "users/" + userObj.uid);
+      const temp = await getDoc(tagRef);
+      setTagArray(temp.data().userAllTags);
     };
-    return (
-        <section>
-            <div className="AddBucket">
-                <form onSubmit={onSubmit}>
-                    <input value={newBucket} type="text" onChange={onChange} placeholder="이루고싶은 일을 적어보세요!" />
-                    <input value={expiredDate} type="date" onChange={onChangeDate} placeholder="마감 기한"/>
-                    <input type="submit" />
-                    <input value={tags} type="text" onChange={onChangeTags} placeholder="공백없이 '#'으로 태그를 추가할 수 있습니다"/>
-                </form>
-                <button type="button">Cancel</button>
-            </div>
-        </section>
-    );
+    getTags();
+  }, []);
+  const getTags = async () => {
+    const tagRef = doc(getFirestore(), "users/" + userObj.uid);
+    const temp = await getDoc(tagRef);
+    setTagArray(temp.data().userAllTags);
+  };
+
+  const onChange = (event) => {
+    event.preventDefault();
+    const {
+      target: { value },
+    } = event;
+    setNewBucket(value);
+  };
+  const onChangeDate = (event) => {
+    event.preventDefault();
+    const {
+      target: { value },
+    } = event;
+    setNewExpiredDate(value);
+  };
+  const onChangeTags = (event) => {
+    event.preventDefault();
+    const {
+      target: { value },
+    } = event;
+    setNewTags(value);
+  };
+  const allInit = () => {
+    setNewBucket("");
+    setNewTags("");
+    getTags();
+    setUserTags(new Map());
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    if (newBucket === "") return;
+    setTagArray(tags.split("#").join(",").split(" ").join(",").split(","));
+
+    await addDoc(bucketRef, {
+      text: newBucket,
+      dateAt: Date.now(),
+      expiredAt: expiredDate,
+      userId: userObj.uid,
+      tags: tagArray,
+    });
+    tagArray.forEach((item) => {
+      if (userTags.has(item) === false) userTags.set(item, true);
+    });
+    allInit();
+  };
+  return (
+    <section>
+      <div className="AddBucket">
+        <form onSubmit={onSubmit}>
+          <input
+            value={newBucket}
+            type="text"
+            onChange={onChange}
+            placeholder="이루고싶은 일을 적어보세요!"
+          />
+          <input
+            value={expiredDate}
+            type="date"
+            onChange={onChangeDate}
+            placeholder="마감 기한"
+          />
+          <input type="submit" />
+          <input
+            value={tags}
+            type="text"
+            onChange={onChangeTags}
+            placeholder="공백없이 '#'으로 태그를 추가할 수 있습니다"
+          />
+        </form>
+        <button type="button">Cancel</button>
+      </div>
+    </section>
+  );
 };
 
 export default AddBucket;
--- src/components/Explore.js
+++ src/components/Explore.js
@@ -1,32 +1,40 @@
-import {getFirestore,collection,query,where, onSnapshot} from "firebase/firestore";
-import {useState} from "react";
-const Explore=()=>{
-    const [userArray,setUserArray]=useState([]);
-    
-    const db=getFirestore();
-    const userRef=collection(db,'users');
-    const q=query(userRef,where("userId","!=",null));
-    onSnapshot(q,(querySnapshot)=>{
-        const userTempArray=[];
-        querySnapshot.forEach((doc)=>{
-            userTempArray.push(doc.data());
-        });
-        setUserArray(userTempArray);
+import {
+  getFirestore,
+  collection,
+  query,
+  where,
+  onSnapshot,
+} from "firebase/firestore";
+import { useState } from "react";
+import openUser from "./openUser";
+const Explore = () => {
+  const [userArray, setUserArray] = useState([]);
+
+  const db = getFirestore();
+  const userRef = collection(db, "users");
+  const q = query(userRef, where("userId", "!=", null));
+  onSnapshot(q, (querySnapshot) => {
+    const userTempArray = [];
+    querySnapshot.forEach((doc) => {
+      userTempArray.push(doc.data());
     });
-    
-    return(
-        <div>
-            {userArray.map((user)=>(
-                <div key={user.userId}>
-                    {(user.user_name==null)?(
-                        <span>익명</span>
-                    ):(
-                        <span>{user.user_name}</span>
-                    )
-                    }
-                </div>
-            ))}
+    setUserArray(userTempArray);
+  });
+  return (
+    <div>
+      {userArray.map((user) => (
+        <div key={user.userId}>
+          {user.user_name == null ? (
+            <span>익명</span>
+          ) : (
+            <div>
+              <span>{user.user_name}</span>
+              <div onClick={openUser({ user })}>click</div>
+            </div>
+          )}
         </div>
-    );
-}
+      ))}
+    </div>
+  );
+};
 export default Explore;
