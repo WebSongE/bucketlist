9462ce1c61e4bae3dd2490b427ce0ee8ce4f0db4
JoJosmin <ead3070@naver.com>
2022-10-07 22:38:57 +0900

    빈 태그 예외 추가

--- src/components/AddBucket.js
+++ src/components/AddBucket.js
@@ -1,259 +1,156 @@
-<<<<<<< HEAD
 import React, { useEffect, useState } from "react";
 import {
-  collection,
-  orderBy,
-  query,
-  updateDoc,
-  getFirestore,
-  getDocs,
-  getDoc,
-  setDoc,
-  addDoc,
-  doc,
+	collection,
+	orderBy,
+	query,
+	updateDoc,
+	getFirestore,
+	getDocs,
+	getDoc,
+	setDoc,
+	addDoc,
+	doc,
 } from "firebase/firestore";
 import { dbService } from "fbase";
 import ShowBucket from "./ShowBucket";
 
 const AddBucket = ({ userObj }) => {
-  const [newBucket, setNewBucket] = useState("");
-  const [tags, setNewTags] = useState("");
-  const [tagArray, setTagArray] = useState([]);
-  const [userTags, setUserTags] = useState(new Map());
-  const [expiredDate, setNewExpiredDate] = useState(new Date());
-  const [buckets, setBuckets] = useState([]);
+	const [newBucket, setNewBucket] = useState("");
+	const [tags, setNewTags] = useState("");
+	const [tagArray, setTagArray] = useState([]);
+	const [userTags, setUserTags] = useState(new Map());
+	const [expiredDate, setNewExpiredDate] = useState(new Date());
+	const [buckets, setBuckets] = useState([]);
 
-  const db = getFirestore();
-  const bucketRef = collection(db, "users/" + userObj.uid + "/buckets");
+	const db = getFirestore();
+	const bucketRef = collection(db, "users/" + userObj.uid + "/buckets");
 
-  useEffect(() => {
-    const getTags = async () => {
-      const tagRef = doc(getFirestore(), "users/" + userObj.uid);
-      const temp = await getDoc(tagRef);
-      setTagArray(temp.data().userAllTags);
-    };
-    getTags();
-  }, []);
-  
-  const getTags = async () => {
-    const tagRef = doc(getFirestore(), "users/" + userObj.uid);
-    const temp = await getDoc(tagRef);
-    console.log("getTags");
-    setTagArray(temp.data().userAllTags);
-  };
-  const onKeyPress = (e) => {
-    if (e.target.value.length !== 0 && e.key === "#") {
-      submitTag();
-    }
-  };
-
-  const onChange = (event) => {
-    event.preventDefault();
-    const {
-      target: { value },
-    } = event;
-    setNewBucket(value);
-  };
-  const onChangeDate = (event) => {
-    event.preventDefault();
-    const {
-      target: { value },
-    } = event;
-    setNewExpiredDate(value);
-  };
-  const allInit = () => {
-    console.log("all init");
-    setNewBucket("");
-    setNewTags("");
-    getTags();
-    setUserTags(new Map());
-  };
-  const submitTag = () => {
-    let updatedTagList = [...tagArray];
-    updatedTagList.push(tags);
-    setTagArray(updatedTagList);
-    setNewTags("");
-  };
-  const deleteTag = (e) => {
-    const deleteTag = e.target.parentElement.firstChild.innerText;
-    const filteredTagList = tagArray.filter((tags) => tags !== deleteTag);
-    setTagArray(filteredTagList);
-  };
-  const onSubmit = async (event) => {
-    event.preventDefault();
-    console.log("onSubmit");
-    console.log(tagArray);
-    await addDoc(bucketRef, {
-      text: newBucket,
-      dateAt: Date.now(),
-      expiredAt: expiredDate,
-      userId: userObj.uid,
-      tags: tagArray,
-    });
-    console.log("successed");
-    allInit();
-    window.location.reload();
-  };
-
-  return (
-    <>
-      <section>
-        <div className="AddBucket">
-          <form onSubmit={onSubmit}>
-            <input
-              value={newBucket}
-              type="text"
-              onChange={onChange}
-              placeholder="이루고싶은 일을 적어보세요!"
-            />
-            <input
-              value={expiredDate}
-              type="date"
-              onChange={onChangeDate}
-              placeholder="마감 기한"
-            />
-            <input type="submit" />
-            <div>
-              {tagArray.map((tags, index) => {
-                return (
-                  <div key={index}>
-                    {tags}
-                    <button onClick={deleteTag}>X</button>
-                  </div>
-                );
-              })}
-              <input
-                type="text"
-                placeholder="공백없이 '#'으로 태그를 추가할 수 있습니다."
-                tagindex={2}
-                onChange={(e) => setNewTags(e.target.value)}
-                value={tags}
-                onKeyPress={onKeyPress}
-              />
-=======
-import React , { useEffect, useState } from "react";
-import {collection, orderBy, query, updateDoc,getFirestore,getDocs, getDoc, setDoc, addDoc,doc} from "firebase/firestore";
-import { dbService } from "fbase";
-import ShowBucket from "./ShowBucket";
-
-const AddBucket = ({userObj}) => {
-    const [newBucket, setNewBucket] = useState("");
-    const [tags, setNewTags] = useState("");
-    const [tagArray,setTagArray]=useState([]);
-    const [userTags,setUserTags]=useState(new Map());
-    const [expiredDate,setNewExpiredDate]=useState(new Date());
-    const [buckets, setBuckets] = useState([]);
-
-    const db=getFirestore();
-    const bucketRef=collection(db,"users/"+userObj.uid+"/buckets");
-    
-    useEffect(()=>{
-        const getTags=async()=>{
-            const tagRef=doc(getFirestore(),"users/"+userObj.uid);
-            const temp=await getDoc(tagRef);
+	useEffect(() => {
+		const getTags = async () => {
+			const tagRef = doc(getFirestore(), "users/" + userObj.uid);
+			const temp = await getDoc(tagRef);
+			if(temp.data().userAllTags){
             setTagArray(temp.data().userAllTags);
         }
-        getTags();
-    },[]);
-    const getTags=async()=>{
-        const tagRef=doc(getFirestore(),"users/"+userObj.uid);
-        const temp=await getDoc(tagRef);
-        console.log("getTags");
-        setTagArray(temp.data().userAllTags);
-    }
-    const onKeyPress = e => {
-        if(e.target.value.length !== 0 && e.key ==="#"){
-        submitTag()
+		};
+		getTags();
+	}, []);
+	const getTags = async () => {
+		const tagRef = doc(getFirestore(), "users/" + userObj.uid);
+		const temp = await getDoc(tagRef);
+		//console.log("getTags");
+        if(temp.data().userAllTags){
+            setTagArray(temp.data().userAllTags);
         }
-    }
+		
+	};
+	const onKeyPress = (e) => {
+		if (e.target.value.length !== 0 && e.key === "#") {
+			submitTag();
+		}
+	};
 
-    const onChange = (event) => {
-        event.preventDefault();
-        const {
-            target: { value },
-        } = event;
-        setNewBucket(value);
-    }
-    const onChangeDate = (event) => {
-        event.preventDefault();
-        const {
-            target: { value },
-        } = event;
-        setNewExpiredDate(value);
-    }
-    const allInit=()=>{
-        console.log("all init");
-        setNewBucket("");
-        setNewTags("");
-        getTags();
-        setUserTags(new Map());
-    }
-    const submitTag = () => {
-        let updatedTagList = [...tagArray]
-        updatedTagList.push(tags)
-        setTagArray(updatedTagList)
-        setNewTags('')
-    }
-    const deleteTag = e => {
-        const deleteTag = e.target.parentElement.firstChild.innerText
-        const filteredTagList  = tagArray.filter(tags => tags !== deleteTag)
-        setTagArray(filteredTagList)
-    }
-    const onSubmit = async (event) => {
-        event.preventDefault();
-        console.log("onSubmit");
-        console.log(tagArray);
-        await addDoc(bucketRef,{
-            text: newBucket,
-            dateAt: Date.now(),
-            expiredAt: expiredDate,
-            userId: userObj.uid,
-            tags: tagArray
-        });
-        console.log("successed");
-        allInit();
-        window.location.reload();
-        
-    };
-    
-    return (
-        <><section>
-            <div className="AddBucket">
-                <form onSubmit={onSubmit}>
-                    <input value={newBucket} type="text" onChange={onChange} required placeholder="이루고싶은 일을 적어보세요!" />
-                    <input value={expiredDate} type="date" onChange={onChangeDate} required placeholder="마감 기한" />
-                    <input type="submit" />
-                    <div>
-                        {tagArray.map((tags, index) => {
-                            return (
-                                <div key={index}>
-                                    {tags}
-                                    <button type="button" onClick={deleteTag}>X</button>
-                                </div>
-                            )
-                        })}
-                        <input type='text'
-                        placeholder="공백없이 '#'으로 태그를 추가할 수 있습니다."
-                        tagindex={2}
-                        onChange={e => setNewTags(e.target.value)}
-                        value={tags}
-                        onKeyPress={onKeyPress}
-                        />
-                    </div>
-                    
-                </form>
-                <button type="button">Cancel</button>
->>>>>>> 1d8c9c932f6a39c03a4efba90f83eb27aa4f3e25
-            </div>
-          </form>
-          <button type="button">Cancel</button>
-        </div>
-      </section>
-      <section>
-        <ShowBucket userObj={userObj} />
-      </section>
-    </>
-  );
+	const onChange = (event) => {
+		event.preventDefault();
+		const {
+			target: { value },
+		} = event;
+		setNewBucket(value);
+	};
+	const onChangeDate = (event) => {
+		event.preventDefault();
+		const {
+			target: { value },
+		} = event;
+		setNewExpiredDate(value);
+	};
+	const allInit = () => {
+		console.log("all init");
+		setNewBucket("");
+		setNewTags("");
+		getTags();
+		setUserTags(new Map());
+	};
+	const submitTag = () => {
+		let updatedTagList = [...tagArray];
+		updatedTagList.push(tags);
+		setTagArray(updatedTagList);
+		setNewTags("");
+	};
+	const deleteTag = (e) => {
+		const deleteTag = e.target.parentElement.firstChild.innerText;
+		const filteredTagList = tagArray.filter((tags) => tags !== deleteTag);
+		setTagArray(filteredTagList);
+	};
+	const onSubmit = async (event) => {
+		event.preventDefault();
+		console.log("onSubmit");
+		console.log(tagArray);
+		await addDoc(bucketRef, {
+			text: newBucket,
+			dateAt: Date.now(),
+			expiredAt: expiredDate,
+			userId: userObj.uid,
+			tags: tagArray,
+		});
+		console.log("successed");
+		allInit();
+		window.location.reload();
+	};
+
+	return (
+		<>
+			<section>
+				<div className="AddBucket">
+					<form onSubmit={onSubmit}>
+						<input
+							value={newBucket}
+							type="text"
+							onChange={onChange}
+							required
+							placeholder="이루고싶은 일을 적어보세요!"
+						/>
+						<input
+							value={expiredDate}
+							type="date"
+							onChange={onChangeDate}
+							required
+							placeholder="마감 기한"
+						/>
+						<input type="submit" />
+						<div>
+							{tagArray.map((tags, index) => {
+								return (
+									<div key={index}>
+										{tags}
+										<button
+											type="button"
+											onClick={deleteTag}
+										>
+											X
+										</button>
+									</div>
+								);
+							})}
+							<input
+								type="text"
+								placeholder="공백없이 '#'으로 태그를 추가할 수 있습니다."
+								tagindex={2}
+								onChange={(e) => setNewTags(e.target.value)}
+								value={tags}
+								onKeyPress={onKeyPress}
+							/>
+						</div>
+						<button type="button">Cancel</button>
+						<button type="button">Cancel</button>
+					</form>
+				</div>
+			</section>
+			<section>
+				<ShowBucket userObj={userObj} />
+			</section>
+		</>
+	);
 };
 
 export default AddBucket;
