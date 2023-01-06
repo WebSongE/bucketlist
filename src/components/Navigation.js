c4202ba30d619fc4008086e35114846259b43307
KimDahui42 <kdie44@gmail.com>
2023-01-06 19:04:28 +0900

    feat:네비게이션 바 색 고정

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,23 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 const Navigation = ({ userObj }) => {
+	const [path, setPath] = useState(window.location.pathname);
+	const clickPath = (clicked) => {
+		console.log(path);
+		setPath(clicked);
+	};
 	return (
 		<nav className="w-screen flex justify-end mt-10">
 			<ul className="flex flex-row gap-x-4 text-center h-10">
-				<li className="hover:bg-lime-200 active:bg-lime-500 outline outline-4 rounded-t-2xl outline-black pt-2 text-xl font-bold bg-white flex-1 w-48">
+				<li
+					onClick={() => clickPath("/explore")}
+					className={`${
+						path === "/explore" ? "bg-lime-400" : "bg-white"
+					} hover:bg-lime-200 active:bg-lime-500 outline outline-4 rounded-t-2xl outline-black pt-2 text-xl font-bold flex-1 w-48`}
+				>
 					<Link to="/explore">explore</Link>
 				</li>
-				<li className="hover:bg-lime-200  active:bg-lime-500 outline outline-4 rounded-t-2xl outline-black pt-2 text-xl font-bold bg-white flex-1 w-48">
+				<li
+					onClick={() => clickPath("/")}
+					className={`${
+						path === "/" ? "bg-lime-400" : "bg-white"
+					} hover:bg-lime-200 active:bg-lime-500 outline outline-4 rounded-t-2xl outline-black pt-2 text-xl font-bold flex-1 w-48`}
+				>
 					<Link to="/">Home</Link>
 				</li>
-				<li className="hover:bg-lime-200  focus:bg-lime-500 outline outline-4 rounded-t-2xl outline-black pt-2 text-xl font-bold bg-white flex-1 w-48">
+				<li
+					onClick={() => clickPath("/profile")}
+					className={`${
+						path === "/profile" ? "bg-lime-400" : "bg-white"
+					} hover:bg-lime-200 active:bg-lime-500 outline outline-4 rounded-t-2xl outline-black pt-2 text-xl font-bold flex-1 w-48`}
+				>
 					<Link to="/profile">
 						{userObj
 							? `${userObj.displayName}의 Profile`
 							: "Profile"}
 					</Link>
 				</li>
-				<li className="hover:bg-lime-200  focus:bg-lime-500 outline outline-4 rounded-t-2xl outline-black pt-2 text-xl font-bold bg-white flex-1 w-48">
+				<li
+					onClick={() => clickPath("/bucket")}
+					className={`${
+						path === "/bucket" ? "bg-lime-400" : "bg-white"
+					} hover:bg-lime-200 active:bg-lime-500 outline outline-4 rounded-t-2xl outline-black pt-2 text-xl font-bold flex-1 w-48`}
+				>
 					<Link to="/bucket">add Bucket</Link>
 				</li>
 			</ul>
--- src/components/bucket.js
+++ src/components/bucket.js
@@ -1,59 +1,55 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import AddBucket from "components/AddBucket";
-import ShowList from "components/showList";
 import {
-  collection,
-  orderBy,
-  query,
-  onSnapshot,
-  getFirestore,
-  getDoc,
+	collection,
+	orderBy,
+	query,
+	getFirestore,
+	getDoc,
 } from "firebase/firestore";
 
 const Bucket = ({ userObj }) => {
-  const [buckets, setBuckets] = useState([]);
-  const q = query(
-    collection(getFirestore(), "buckets"),
-    orderBy("dateAt", "desc")
-  );
-  onSnapshot(q, (snapshot) => {
-    const newArray = snapshot.docs.map((document) => ({
-      id: document.id,
-      ...document.data(),
-    }));
-    setBuckets(newArray);
-  });
-  const bucketsRef = collection(
-    getFirestore(),
-    "users/" + userObj.uid + "/buckets"
-  );
-  //const q = query(bucketsRef, orderBy('dateAt','desc'));
-  const temp = getDoc(bucketsRef);
-  const tempBuckets = [];
-  temp.forEach((doc) => {
-    tempBuckets.push({
-      id: doc.id,
-      text: doc.data().text,
-      dateAt: doc.data().dateAt,
-      expiredAt: doc.data().expiredAt,
-      tags: doc.data().tags,
-      userId: doc.data().userId,
-      complete:doc.data().complete,
-    });
-  });
+	const [buckets, setBuckets] = useState([]);
+	const bucketsRef = collection(
+		getFirestore(),
+		"users/" + userObj.uid + "/buckets"
+	);
+	useEffect((bucketsRef) => {
+		const q = query(bucketsRef, orderBy("dateAt", "desc"));
+		console.log("print");
+		const newArray = q.docs.map((document) => ({
+			id: document.id,
+			...document.data(),
+		}));
+		setBuckets(newArray);
+	}, []);
 
-  return (
-    <div>
-      <AddBucket userObj={userObj} />
-      <div>
-        {buckets.map((bucket) => (
-          <div key={bucket.id}>
-            <div>{bucket.text}</div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+	const temp = getDoc(bucketsRef);
+	const tempBuckets = [];
+	temp.forEach((doc) => {
+		tempBuckets.push({
+			id: doc.id,
+			text: doc.data().text,
+			dateAt: doc.data().dateAt,
+			expiredAt: doc.data().expiredAt,
+			tags: doc.data().tags,
+			userId: doc.data().userId,
+			complete: doc.data().complete,
+		});
+	});
+
+	return (
+		<div>
+			<AddBucket userObj={userObj} />
+			<div>
+				{buckets.map((bucket) => (
+					<div key={bucket.id}>
+						<div>{bucket.text}</div>
+					</div>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default Bucket;
