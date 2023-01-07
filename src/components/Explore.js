7d2d455c8a68763fb921148898ec813da3dcfddb
JoJosmin <ead3070@naver.com>
2023-01-07 22:12:11 +0900

    다른 유저 버킷리스트

--- src/components/Explore.js
+++ src/components/Explore.js
@@ -32,11 +32,11 @@ const Explore = ({ user }) => {
 				<div key={user.userId} class="flex flex-auto justify-items-start space-x-3">
 					<img src="pic/pen.png" />
 					{user.user_name == null ? (
-						<Link to={`/user_detail/${user.userId}`} class="my-10">
+						<Link to={`/user_detail/${user.userId}`} class="py-10">
 							<span class= "font-extrabold">익명</span>
 						</Link>
 					) : (
-						<span class= "my-10 font-extrabold">{user.user_name}</span>
+						<span class= "py-10 font-extrabold">{user.user_name}</span>
 					)}
 				</div>
 			))}
