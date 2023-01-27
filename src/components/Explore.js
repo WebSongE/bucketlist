23bf152f32b3efc9a623d40fda6a7e0d65d9a7fa
KimDahui42 <kdie44@gmail.com>
2023-01-27 23:36:36 +0900

    fix: layout css

--- src/components/Explore.js
+++ src/components/Explore.js
@@ -28,21 +28,28 @@ const Explore = ({ user }) => {
 			<div className="font-bold text-center px-5 py-3 text-2xl ">
 				다른 사람들의 버킷리스트를 구경해보세요!
 			</div>
-			<div className="flex flex-col items-center">
+			<div className="flex flex-col">
 				{userArray.map((user) => (
-					<div key={user.userId} class="flex flex-auto justify-items-start ml-10 my-3">
+					<div
+						key={user.userId}
+						class="flex flex-auto items-center ml-10 my-3"
+					>
 						<img src="pic/pen.png" />
 						{user.user_name == null ? (
-							<Link to={`/user_detail/${user.userId}`} class="pl-3 py-10">
-								<span class= "font-extrabold">익명</span>
+							<Link
+								to={`/user_detail/${user.userId}`}
+								class="pl-3 py-10"
+							>
+								<span class="font-extrabold">익명</span>
 							</Link>
 						) : (
-							<span class= "pl-3 py-10 font-extrabold">{user.user_name}</span>
+							<span class="pl-3 py-10 font-extrabold">
+								{user.user_name}
+							</span>
 						)}
 					</div>
 				))}
 			</div>
-			
 		</div>
 	);
 };
