4e76485428dc623adfcca45e62b2cf6ee858d130
KimDahui42 <kdie44@gmail.com>
2023-02-10 22:55:21 +0900

    feat: 좋아요 누른 상태 적용

--- src/components/LikeButton.js
+++ src/components/LikeButton.js
@@ -1,6 +1,6 @@
 import heart from "static/images/heart.svg";
 import emptyHeart from "static/images/emptyHeart.svg";
-import { useState, useEffect } from "react";
+import { useState, useLayoutEffect } from "react";
 import { getAuth } from "firebase/auth";
 import {
 	getFirestore,
@@ -16,25 +16,24 @@ const LikeButton = ({ bucket, user }) => {
 	const isClick = () => setClick((click) => !click);
 	const currentUser = getAuth().currentUser;
 
-	// useEffect(() => {
-	// 	const setClicked = async () => {
-	// 		const userRef = doc(getFirestore(), `users/${currentUser.uid}`);
-	// 		const temp = await getDoc(userRef);
-	// 		const likedArray = temp.data().userLiked;
-	// 		//사용자가 좋아요를 누른 리스트에 버킷이 있다면 true 아니면 false
-	// 		setClick(likedArray.find((e) => e === bucket) !== undefined);
-	// 	};
-	// 	setClicked();
-	// }, []);
+	useLayoutEffect(() => {
+		const setClicked = async () => {
+			const userRef = doc(getFirestore(), `users/${currentUser.uid}`);
+			const temp = await getDoc(userRef);
+			const likedArray = temp.data().userLiked;
+			//사용자가 좋아요를 누른 리스트에 버킷이 있다면 true 아니면 false
+			setClick(likedArray.find((e) => e === bucket) !== undefined);
+		};
+		setClicked();
+	}, []);
 
 	const onLikeClick = (e) => {
 		isClick();
-		if (click) like(e.target.value);
-		else unlike(e.target.value);
+		if (click) unlike(e.target.value);
+		else like(e.target.value);
 	};
 
 	const like = async () => {
-		console.log("click");
 		const bucketRef = doc(
 			getFirestore(),
 			`users/${user}/buckets/${bucket}`
@@ -43,13 +42,11 @@ const LikeButton = ({ bucket, user }) => {
 			like: arrayUnion(currentUser.uid),
 		});
 		await updateDoc(doc(getFirestore(), `users/${currentUser.uid}`), {
-			like: arrayUnion(bucket),
+			userLiked: arrayUnion(bucket),
 		});
-		console.log(bucket);
 	};
 
 	const unlike = async () => {
-		console.log("unclick");
 		const bucketRef = doc(
 			getFirestore(),
 			`users/${user}/buckets/${bucket}`
@@ -58,7 +55,7 @@ const LikeButton = ({ bucket, user }) => {
 			like: arrayRemove(currentUser.uid),
 		});
 		await updateDoc(doc(getFirestore(), `users/${currentUser.uid}`), {
-			like: arrayRemove(bucket),
+			userLiked: arrayRemove(bucket),
 		});
 	};
 	return (
